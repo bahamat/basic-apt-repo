fd652998b6665211e8520cfa0383853b4c070aee
Brian Bennett <bahamat@digitalelf.net>
2013-11-08 12:19:25 -0800

    More hints for GPGOPTS

--- gpg.mk
+++ gpg.mk
@@ -1,4 +1,5 @@
 # Optionally include extra options for gpg here.
+# You can combine multiple options on a single line.
 #
 # Include the passphrase directly:
 #GPGOPTS=--passphrase insecure
@@ -6,6 +7,9 @@
 # Include the passphrase more securely.
 # Don't put this file in your web root!
 #GPGOPTS=--passphrase-file <filename>
+#
+# Include an alternate signing key.
+#GPGOPTS=--homedir /path/to/gpg/home
 #
 # By default, you get prompted:
 GPGOPTS=
